871c52811cd3623908df759e94e5e56c216a0cb6
itsjustherain <eduivi0078@gmail.com>
2026-04-07 19:43:38 +0200

    Extract method sumar para separar la lógica de cálculo

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,11 @@
 public class Calculadora {
     public void imprimirSuma(int numeroUno, int numeroDos) {
-        int resultado = numeroUno + numeroDos;
+        int resultado = sumar(numeroUno, numeroDos);
         System.out.println("Resultado: " + resultado);
+    }
+
+    private int sumar(int numeroUno, int numeroDos) {
+        int resultado = numeroUno + numeroDos;
+        return resultado;
     }
 }
